70a56ad37e1dbdc45e5ab6c47b3fc14ff853f7b5
abdulshukur1vr <abdulshukur1vr@gmail.com>
2024-09-14 19:02:35 -0500

    Add files via upload

---
+++ include/utils.hpp
@@ -0,0 +1,11 @@
+// utils.hpp
+
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+#include <string>
+
+void display_spinner(bool& stop); 
+std::string to_lowercase(const std::string& input);
+
+#endif // __UTILS_HPP__
